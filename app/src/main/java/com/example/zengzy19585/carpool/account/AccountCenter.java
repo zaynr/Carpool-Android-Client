322b68389016f503a1f0ce190bf8fbfc186f147c
Zengyu Zeng <zayn@mail.ustc.edu.cn>
2017-07-29 16:50:41 +0800

    new feature: user info update

--- app/src/main/java/com/example/zengzy19585/carpool/account/AccountCenter.java
+++ app/src/main/java/com/example/zengzy19585/carpool/account/AccountCenter.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.baidu.mapapi.map.Text;
 import com.example.zengzy19585.carpool.R;
 import com.example.zengzy19585.carpool.utils.SharedPreferencesUtil;
 import com.loopj.android.http.AsyncHttpClient;
@@ -21,7 +24,36 @@ public class AccountCenter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_center);
         userInfo = new SharedPreferencesUtil(getApplicationContext(), "userInfo");
-        Button logOut = (Button) findViewById(R.id.log_out);
+        final Button logOut = (Button) findViewById(R.id.log_out);
+        final Button update = (Button) findViewById(R.id.update);
+        final Button password = (Button) findViewById(R.id.password);
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText name, sex, mobile_num;
+                name = (EditText) findViewById(R.id.textView);
+                sex = (EditText) findViewById(R.id.textView3);
+                mobile_num = (EditText) findViewById(R.id.textView2);
+                boolean flag;
+                if(update.getText().toString().contains("更改")){
+                    flag = true;
+                    update.setText("提交");
+                }
+                else{
+                    flag = false;
+                    update.setText("更改基本信息");
+                }
+                name.setFocusableInTouchMode(flag);
+                sex.setFocusableInTouchMode(flag);
+                mobile_num.setFocusableInTouchMode(flag);
+            }
+        });
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
