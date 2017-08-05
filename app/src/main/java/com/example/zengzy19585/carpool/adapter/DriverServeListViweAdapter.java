dc15c74f91577ee4d1bf0d102032b55a3f453bbb
Zengyu Zeng <zayn@mail.ustc.edu.cn>
2017-08-05 23:07:11 +0800

    new feature

---
+++ app/src/main/java/com/example/zengzy19585/carpool/adapter/DriverServeListViweAdapter.java
@@ -0,0 +1,72 @@
+package com.example.zengzy19585.carpool.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.zengzy19585.carpool.R;
+import com.example.zengzy19585.carpool.entity.Friends;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zaynr on 2017/8/5.
+ */
+
+public class DriverServeListViweAdapter extends BaseAdapter {
+    private ArrayList<Friends> friendses;
+    private Context context;
+
+    private class ViewHolder{
+        TextView friendName, friendContact, serveCount;
+    }
+
+    public DriverServeListViweAdapter(Context context, ArrayList<Friends> friendses){
+        this.context = context;
+        this.friendses = friendses;
+    }
+
+    @Override
+    public int getCount() {
+        return friendses.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return friendses.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Friends friends = friendses.get(i);
+        ViewHolder viewHolder;
+        if(view == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(R.layout.show_friend_list, viewGroup, false);
+            viewHolder.friendContact = view.findViewById(R.id.friend_contact);
+            viewHolder.friendName = view.findViewById(R.id.friend_name);
+            viewHolder.serveCount = view.findViewById(R.id.serve_count);
+
+            view.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        if(friendses.get(i).getType().contains("driver")){
+            viewHolder.friendName.setText(friendses.get(i).getCall_serial());
+            viewHolder.friendContact.setText(friendses.get(i).getCall_mobile());
+            viewHolder.serveCount.setText(friendses.get(i).getServe_count());
+        }
+        return view;
+    }
+}
--- app/src/main/java/com/example/zengzy19585/carpool/entity/Friends.java
+++ app/src/main/java/com/example/zengzy19585/carpool/entity/Friends.java
@@ -5,7 +5,58 @@ package com.example.zengzy19585.carpool.entity;
  */
 
 public class Friends {
-    String userial1, userial2, serial_num;
+    private String userial1, userial2, serial_num, type, invokeName;
+
+    private int serve_count;
+    private String rec_mobile_num, call_serial, call_mobile;
+
+    public String getCall_mobile() {
+        return call_mobile;
+    }
+
+    public void setCall_mobile(String call_mobile) {
+        this.call_mobile = call_mobile;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getInvokeName() {
+        return invokeName;
+    }
+
+    public void setInvokeName(String invokeName) {
+        this.invokeName = invokeName;
+    }
+
+    public int getServe_count() {
+        return serve_count;
+    }
+
+    public void setServe_count(int serve_count) {
+        this.serve_count = serve_count;
+    }
+
+    public String getRec_mobile_num() {
+        return rec_mobile_num;
+    }
+
+    public void setRec_mobile_num(String rec_mobile_num) {
+        this.rec_mobile_num = rec_mobile_num;
+    }
+
+    public String getCall_serial() {
+        return call_serial;
+    }
+
+    public void setCall_serial(String call_serial) {
+        this.call_serial = call_serial;
+    }
 
     public String getUserial1() {
         return userial1;
