9d77cd8321baff6b091d7fa8877ba7574fd4cb7c
Zengyu Zeng <zayn@mail.ustc.edu.cn>
2017-08-14 19:32:12 +0800

    fix permission

--- app/src/main/java/com/example/zengzy19585/carpool/MainActivity.java
+++ app/src/main/java/com/example/zengzy19585/carpool/MainActivity.java
@@ -241,11 +241,13 @@ public class MainActivity extends AppCompatActivity
 
         if(mContext.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // 申请一个（或多个）权限，并提供用于回调返回的获取码（用户定义）
-            requestPermissions( new String[] { Manifest.permission.ACCESS_COARSE_LOCATION}, ACCESS_COARSE_LOCATION);
+            Toast.makeText(getApplicationContext(), "请打开定位权限！", Toast.LENGTH_SHORT).show();
+            finish();
         }
         if(mContext.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // 申请一个（或多个）权限，并提供用于回调返回的获取码（用户定义）
-            requestPermissions( new String[] { Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION);
+            Toast.makeText(getApplicationContext(), "请打开定位权限！", Toast.LENGTH_SHORT).show();
+            finish();
         }
     }
 
